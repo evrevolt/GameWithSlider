5e5b585cb97e76c63f7e8316253b895723a4dd33
Gennady Vedernikov <revoltlight@icloud.com>
2022-11-14 19:33:43 +0300

    Initial Commit

---
+++ GameWithSlider/GameWithSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameWithSliderApp.swift
+//  GameWithSlider
+//
+//  Created by Геннадий Ведерников on 14.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GameWithSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
